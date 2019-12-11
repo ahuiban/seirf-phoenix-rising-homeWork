c806c849e2f2aa80c772d03a28d234e40790e6a2
Andrei Huiban <ahuiban@gmail.com>
2019-12-11 12:49:04 -0500

    current week homework

---
+++ 02. w01d03/loops.js
@@ -0,0 +1,27 @@
+/*
+1.
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+}
+2.
+for (let i = 99; i >= 0; i--) {
+    console.log(i);
+}
+3.
+for (let i = 0; i < 99; i += 2) {
+    console.log(i);
+}
+4.
+for (let i = 98; i >= 0; i -= 2) {
+    console.log(i);
+}
+5.
+for (let i=1; i <= 99; i += 2) {
+    console.log(i);
+}
+6.
+for (let i = 99; i >= 1; i -= 2) {
+    console.log(i);
+}
+7.
+
