939a8a17dfac982026dcbde66c079f24e3485a81
Andrei Huiban <ahuiban@gmail.com>
2019-12-16 18:28:52 -0500

    verbal questions completed

---
+++ 04. w02d03/solutions.js
@@ -0,0 +1,13 @@
+/*
+
+Verbal Questions
+
+1. What is the difference between a parameter and an argument?
+R: A parameter is what appears in the definition of the method. 
+An argument is the instance passed to the method during runtime.
+
+2. Within a function, what is the difference between return and console.log?
+R: Within a function, we use RETURN to output a value back to where it was called; 
+we use CONSOLE.LOG to inspect values, for debugging purposes.
+
+
