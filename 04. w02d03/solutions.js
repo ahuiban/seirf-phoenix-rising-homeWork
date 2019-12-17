5a48ffd808f6f832892701229a1105c7fa846495
Andrei Huiban <ahuiban@gmail.com>
2019-12-16 20:30:24 -0500

    sum array completed

--- 04. w02d03/solutions.js
+++ 04. w02d03/solutions.js
@@ -11,7 +11,12 @@ R: Within a function, we use RETURN to output a value back to where it was calle
 we use CONSOLE.LOG to inspect values, for debugging purposes.
 
 
-*/
+//// Palindrome ////
+R: 
+- The toLowerCase() method to return the calling string value converted to lowercase.
+- The split() method splits a String object into an array of strings by separating the string into sub strings.
+- The reverse() method reverses an array in place. The first array element becomes the last and the last becomes the first.
+- The join() method joins all elements of an array into a string.
 
 const checkPalindrome = (str) => {
 
@@ -23,3 +28,31 @@ return false
 };
 
 console.log(checkPalindrome("RaDar"));
+
+//// SUM ARRAY ////
+Write a function sumArray that takes an array as an argument. The array should contain numbers. The function should return the sum of the numbers in the array. 
+Use a for loop within the function to iterate over the array and sum the contents.
+R:
+
+const sumArray = (array) => {
+let sum = 0;
+
+for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+}
+return sum
+}
+
+console.log(sumArray([10,11,12,13,17]));
+
+*/
+
+
+const sumArray = [1, 2, 3, 5, 9];
+
+for (let i = 0, sum = 0; i < sumArray.length; sum += sumArray[i++]);
+
+console.log(sum);
+
+
+
