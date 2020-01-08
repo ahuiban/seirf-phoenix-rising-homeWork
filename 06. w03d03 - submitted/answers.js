dcac13015978aa5df5d12f710889a738ac469710
Andrei Huiban <ahuiban@gmail.com>
2020-01-08 04:24:36 -0500

    current week homework

---
+++ 06. w03d03 - submitted/answers.js
@@ -0,0 +1,61 @@
+/*
+
+/// Answer the Following ///
+
+For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:
+
+e.g. A computer that can be either on or off.
+=> datatype: boolean
+=> data structure example: const computer = true;
+
+
+A light switch that can be either on or off.
+=> datatype: boolean
+=> data structure example: const light = true;
+
+A user's email address.
+=> datatype: string
+=> data structure example: user@domain.com
+
+A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+=> datatype: object
+=> data structure example: const spaceship = {
+    "hull": 'idk01',
+    "laserBlaster": 'idk02',
+    "tractorBeam": 'idk03',
+    "warpDrive": 'idk04',
+}
+
+A list of student names from our class.
+=> datatype: array
+=> data structure example: const phoenixRising= ['Andrei', 'Chris', 'Axel']
+
+A list of student names from our class, each with a location.
+=> datatype: array of objects
+=> data structure example: const phoenixRising = [
+    {
+    "name": 'Andrei',
+    "location": 'Atlanta"
+}
+]
+
+A list of student names from our class, each with a location and each with a list of favorite tv shows.
+=> datatype: array of objects with an inner array
+=> data structure example: const phoenixRising = [
+    {
+        "name": 'Andrei',
+        "location": "ATLANTA",
+        "favoriteTvShows": ['Mind of Mencia', 'NelkFilmz']
+
+    }
+]
+
+
+//// TAKE IT EASY ////
+
+Make an array that holds all of the colors of the rainbow.
+const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+Write code that will access "blue" from the rainbow array.
+Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+Write code that will access your hobby from the object that you just created.
