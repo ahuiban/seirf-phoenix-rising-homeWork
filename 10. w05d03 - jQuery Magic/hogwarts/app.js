1bc41b05796bacac9164a55ac49c35df30aa5602
Andrei Huiban <ahuiban@gmail.com>
2020-01-21 11:49:28 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- 10. w05d03 - jQuery Magic/hogwarts/app.js
+++ 10. w05d03 - jQuery Magic/hogwarts/app.js
@@ -2,6 +2,7 @@
 //     console.log('oops! I still have to link my jQuery properly!');
 // } else { console.log('I did it! I linked jQuery and this js file!') };
 
+//Year 1
 
 $(() => {
     $('#container') // Query for your div with the id of container
@@ -13,5 +14,32 @@ $(() => {
     $("#container").append($h1) // Append your $h1 to your $container
 
 
+    //year2 
+    // Following what you did in Year 1, add the following:
+
+    // h2 element with your name
+    const $h2 = $("<h2>")
+    $h2.text("Andrei")
+    $("#container").append($h2)
+
+    // h3 element with your house (Hufflepuff, Gryffindor, Ravenclaw or Slytherin)
+
+    const $h3 = $("<h3>")
+    $h3.text("Gryffindor")
+    $("#container").append($h3)
+
+    //h4 element with your pet's name
+    //this h4 element should have a class with a value of your pet type (owl, cat, toad, etc)
+    //h4 element with your wand (You can make it whatever you want.
+    //Need help coming up with one? Here is one of many websites to help you find your wand playbuzz.com )
+
+    const $h4 = $("<h4>")
+    $h4.addClass("dog").text("Griff")
+    $h4.text("Hawthorn Wand with Dragon Heartstring Core")
+    $("#container").append($h4)
 
 });
+
+
+
+
