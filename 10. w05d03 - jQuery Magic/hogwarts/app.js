6949af527a925cdbf3d0f8646b6136172a1a8cb1
Andrei Huiban <ahuiban@gmail.com>
2020-01-21 11:07:09 -0500

    Commit 1 - I added my first element and it was fun

--- 10. w05d03 - jQuery Magic/hogwarts/app.js
+++ 10. w05d03 - jQuery Magic/hogwarts/app.js
@@ -3,3 +3,15 @@
 // } else { console.log('I did it! I linked jQuery and this js file!') };
 
 
+$(() => {
+    $('#container') // Query for your div with the id of container
+    const $container = $('#container') // set it to a variable named $container
+    console.log($container) // console.log $container
+    const $h1 = $("<h1>") // Create an <h1> element and set it to a variable called $h1
+    console.log($h1)
+    $h1.text("Hogwarts") // Add some text inside the h1 element. Example text: 'Hogwarts' (you have to .append the h1 to be visible on the page)
+    $("#container").append($h1) // Append your $h1 to your $container
+
+
+
+});
