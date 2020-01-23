a08984b220a111ee28783be81571eba0a78172bf
Andrei Huiban <ahuiban@gmail.com>
2020-01-23 08:49:31 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- 10. w05d03 - jQuery Magic/hogwarts/app.js
+++ 10. w05d03 - jQuery Magic/hogwarts/app.js
@@ -6,39 +6,34 @@
 
 $(() => {
     $('#container') // Query for your div with the id of container
-    const $container = $('#container') // set it to a variable named $container
-    console.log($container) // console.log $container
-    const $h1 = $("<h1>") // Create an <h1> element and set it to a variable called $h1
+    const $container = $('#container'); // set it to a variable named $container
+    console.log($container); // console.log $container
+    const $h1 = $("<h1>").attr("id", "header"); // Create an <h1> element and set it to a variable called $h1
     console.log($h1)
-    $h1.text("Hogwarts") // Add some text inside the h1 element. Example text: 'Hogwarts' (you have to .append the h1 to be visible on the page)
-    $("#container").append($h1) // Append your $h1 to your $container
+    $h1.text("Hogwarts"); // Add some text inside the h1 element. Example text: 'Hogwarts' (you have to .append the h1 to be visible on the page)
+    $("#container").append($h1); // Append your $h1 to your $container
 
 
     //year2 
     // Following what you did in Year 1, add the following:
 
     // h2 element with your name
-    const $h2 = $("<h2>")
-    $h2.text("Andrei")
-    $("#container").append($h2)
+    const $name = $("<h2>").text("Andrei Huiban")
+    $($container).append($name)
 
     // h3 element with your house (Hufflepuff, Gryffindor, Ravenclaw or Slytherin)
 
-    const $h3 = $("<h3>")
-    $h3.text("Gryffindor")
-    $("#container").append($h3)
+    const $house = $("<h3>").text("Gryffindor");
+    $($container).append($house);
 
-    //h4 element with your pet's name
-    //this h4 element should have a class with a value of your pet type (owl, cat, toad, etc)
-    //h4 element with your wand (You can make it whatever you want.
-    //Need help coming up with one? Here is one of many websites to help you find your wand playbuzz.com )
+    //h4 element with your pet's name. this h4 element should have a class with a value of your pet type (owl, cat, toad, etc)
+    const $pet = $("<h4>").text("Camden").addClass("cat");
+    $($container).append($pet);
 
-    const $h4 = $("<h4>")
+    //h4 element with your wand (You can make it whatever you want).
 
-    $h4.addClass("cat").text("Camden")
-    $h4.text("Hawthorn Wand with Dragon Heartstring Core")
-    $("#container").append($h4)
-
+    const $wand = $("<h4>").text("Hawthorn Wand with Dragon Heartstring Core").addClass("wand");
+    $($container).append($wand);
 
 
     //     Year 3
