8fc33a1c20c8a2dcc8e8034908d54e649e87f359
Andrei Huiban <ahuiban@gmail.com>
2020-01-29 19:43:53 -0500

    Tic Tac Toe Gaame

--- 13. w07d02 - Tic tac Toe /Tic Tac Toe/app.js
+++ 13. w07d02 - Tic tac Toe /Tic Tac Toe/app.js
@@ -1,6 +1,17 @@
 console.log("Working")
 
+
+
+const squareArray = new Array();
+
+for (let x = 0; x <= 8; x++) {
+    prompt["square" + x] = "";
+};
+
 $(function () {
     const character = prompt("Do you prefer X or 0?");
+    $(".square").bind("click", function () {
+        const squareID = $(this).attr("id");
+    });
 })
 
