f64f8f0feec2077f7111687739e77d49e3fbac84
Andrei Huiban <ahuiban@gmail.com>
2020-01-30 01:55:55 -0500

    Tic Tac Toe

--- 13. w07d02 - Tic tac Toe /Tic Tac Toe/app.js
+++ 13. w07d02 - Tic tac Toe /Tic Tac Toe/app.js
@@ -1,17 +1,43 @@
-console.log("Working")
-
-
-
 const squareArray = new Array();
+const characterUser;
+setUserCharacter();
+const characterAuto = (characterUser == "x") ? "0" : "X";
 
 for (let x = 0; x <= 8; x++) {
-    prompt["square" + x] = "";
+    squareArray["square" + x] = "";
 };
 
 $(function () {
-    const character = prompt("Do you prefer X or 0?");
-    $(".square").bind("click", function () {
+    $(".scuare").blind("click", function () {
         const squareID = $(this).attr("id");
+
+        if (!squareArray[squareID]) {
+            squareArray[squareID] = characterUser;
+            $(this).html(characterUser);
+            addComputerchar();
+        }
     });
-})
+});
+
+function setUsercharacter() {
+    characterUser = prompt("What do you choose? X or 0?");
+    if (characterUser != "x" && characterUser != "0") {
+        setUserCharacter();
+    }
+}
+
+function addComputerchar() {
+    randNr = Math.floor((Math.random() * 8) + 1);
+
+    if (squareArray["square" + randNr] == "") {
+        squareArray["square" + randNr] = characterAuto;
+        $("#square" + randNr).html(characterAuto);
+    } else {
+        addComputerchar();
+    }
+}
+
+
+
+
 
