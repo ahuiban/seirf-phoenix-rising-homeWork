29bc22f9ee7fa02f59fc548021c3607f1743d353
Andrei Huiban <ahuiban@gmail.com>
2020-02-19 08:46:40 -0500

    Poke Express

--- 15.w09d03-PokeExpress/pokemon_app/server.js
+++ 15.w09d03-PokeExpress/pokemon_app/server.js
@@ -4,14 +4,26 @@ const app = express();
 
 const port = 3000;
 
+
+
+const pokemon = require('./models/pokemon.js'
+)
+
+app.get('/pokemon/:id', (req, res) => {
+    res.render('show.ejs',
+        { pokemon: pokemon[req.params.id] })
+})
+
+
+app.get('/pokemon', (req, res) => {
+    res.render('index.ejs',
+        { pokemon: pokemon })
+})
+
+
 app.get("/", (req, res) => {
     res.send("Welcome to the Pokemon App!");
 });
-
-
-
-
-
 
 
 
