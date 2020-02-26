ddb863aec15b096a90179155668f1b539bf25c83
Andrei Huiban <ahuiban@gmail.com>
2020-02-26 08:42:16 -0500

    Budgtr

--- 16.w10d02-Budgtr/server.js
+++ 16.w10d02-Budgtr/server.js
@@ -5,9 +5,19 @@ const port = 3000;
 //configured express.static
 app.use(express.static(__dirname + '/public'));
 
+//db file
+const Budget = require('./models/budget.js'
+)
 
 
+//routes
 
+app.use((req, res, next) => {
+    console.log('all routes')
+    next()
+})
+
+app.use(express.urlencoded({ extended: false }));
 
 
 
