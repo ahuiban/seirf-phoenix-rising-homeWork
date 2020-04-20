9031b5f683bdf4edc104185c6773c551974dd224
Andrei Huiban <ahuiban@gmail.com>
2020-04-20 19:24:54 -0400

    Ruby Basics Practice

---
+++ 22.w18d02-Ruby-Basics-Practice/student_solution.rb
@@ -0,0 +1,23 @@
+# Hello World
+# Print "Hello World" to the console
+p "Hello World"
+
+# After you have printed Hello World:
+
+adjective = "Big Bad"
+
+# Interpolate the adjective variable into the Hello World string using #{}
+# Expected output:
+
+# => Hello Big Bad World
+
+p "Hello #{adjective} World"
+
+# Save "Hello World" to a variable. Without changing the letters in your code manually, 
+#permanently change "Hello World" to all uppercased letters.
+
+hello = "Hello World"
+hello.upcase!
+p hello
+
+
