b21aaa8ebbb92e40c6956286034eb8fc73d8a974
Andrei Huiban <ahuiban@gmail.com>
2020-04-25 16:54:54 -0400

    Commit 8: Can POST new Tweets

--- 25.w20d2-tweet_R/tweeter_app_api/app/controllers/tweets_controller.rb
+++ 25.w20d2-tweet_R/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -6,13 +6,23 @@ class TweetsController < ApplicationController
 
     def show
         found_tweet = Tweet.find(params[:id])
-        render json: { status: 200, tweet: found_tweet}
+        render json: { status: 200, tweet: tweet}
     end
 
+    def create
+        tweet = Tweet.new(tweet_params)
+        if tweet.save
+            render(status: 201, json: { tweet: tweet })
+        else 
+            render(status: 422, json: { tweet: tweet })
+    end
+end
 
 
+private
+def tweet_params
+    params.require(:tweet).permit(:title, :content, :author)
 
-
-
+end
 end
 
