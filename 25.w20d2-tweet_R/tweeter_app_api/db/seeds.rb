a3e508dd593b89648df654bfedc1908850d44c12
Andrei Huiban <ahuiban@gmail.com>
2020-04-25 16:22:34 -0400

    Commit 4: Created model and seeded db

--- 25.w20d2-tweet_R/tweeter_app_api/db/seeds.rb
+++ 25.w20d2-tweet_R/tweeter_app_api/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Tweet.create({
+  title: "Just found this",
+  content: "the square of the hypotenuse is equal to the sum of the squares of the other two sides",
+  author: "Pythagoras570"
+})
+
+Tweet.create({
+  title: "I'm walkin' here",
+  content: "Hey, I'm walkin' here!",
+  author: "Nicky62"
+})
